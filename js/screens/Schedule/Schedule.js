916cb88a35b4757ed97cbcc85f2c3fa8d2b54ee2
Andre Moura <andremarquesmoura@gmail.com>
2019-05-28 16:02:18 -0700

    Populate the Schedule screen with data

--- js/screens/Schedule/Schedule.js
+++ js/screens/Schedule/Schedule.js
@@ -1,11 +1,31 @@
 import React, { Component, Fragment } from "react";
-import { Text, View } from "react-native";
+import { ItemSeparatorComponent, SectionList, Text, View } from "react-native";
+import moment from "moment";
 import { styles } from "./styles";
 
-const Schedule = data => {
+const Schedule = ({ data }) => {
+  console.log(data);
   return (
     <View style={styles.container}>
       <Text>This is the Schedule screen...</Text>
+      <SectionList
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={{ fontWeight: "bold" }}>
+            {moment(title).format("h:mm a")}
+          </Text>
+        )}
+        renderItem={({ item }) => (
+          <View key={item.id}>
+            <Text>{item.title}</Text>
+            <Text>{item.location}</Text>
+          </View>
+        )}
+        sections={data}
+        keyExtractor={item => item.id}
+        ItemSeparatorComponent={() => (
+          <View style={{ borderBottomColor: "#999", borderBottomWidth: 1 }} />
+        )}
+      />
     </View>
   );
 };
--- js/screens/Schedule/ScheduleContainer.js
+++ js/screens/Schedule/ScheduleContainer.js
@@ -1,13 +1,43 @@
 import React, { Component } from "react";
+import { Text } from "react-native";
 import Schedule from "./Schedule";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import { formatSessionData } from "../../helpers";
 
 class ScheduleContainer extends Component {
   static navigationOptions = {
     title: "Schedule"
   };
   render() {
-    return <Schedule />;
+    return (
+      <Query query={GET_CONDUCT_ITEMS}>
+        {({ loading, data }) => {
+          if (loading || !data) return <Text>Loading...</Text>; // replace by Loader component
+          return <Schedule data={formatSessionData(data.allSessions)} />;
+        }}
+      </Query>
+    );
   }
 }
+
+const GET_CONDUCT_ITEMS = gql`
+  query {
+    allSessions(orderBy: startTime_ASC) {
+      id
+      title
+      location
+      description
+      speaker {
+        id
+        name
+        bio
+        image
+        url
+      }
+      startTime
+    }
+  }
+`;
 
 export default ScheduleContainer;
